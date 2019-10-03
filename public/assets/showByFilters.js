77f0ab022515c0e6a14757af9a7a9a6d8919d608
noel an <asiapro69@hotmail.fr>
2019-10-03 06:42:33 +0200

    some responsive

--- public/assets/showByFilters.js
+++ public/assets/showByFilters.js
@@ -1,15 +1,20 @@
 function showByFilters() {
-	console.log("hey")
-	let checkBoxes = document.getElementsByclass('myCheckbox')
-	checkBoxes.forEach(function(element) {
-		element.addEventListener("Click", function() {
-			console.log("hey")
-			checkBoxes.forEach(function(checkBox) {
-				console.log("hey")
-				if(element.checked == true);
-				console.log("hey")
-			})
-		})
-	})
+    let checkBoxes = document.getElementsByClassName('myCheckbox')
+    let currentParameters = ""
+    let myType = document.getElementById('myType').textContent.substr(4)
+    let url = ""
+    console.log(myType)
+    for (var i = 0; i < checkBoxes.length; i++) {
+        checkBoxes[i].addEventListener("click", function() {
+            for (var j = 0; j < checkBoxes.length; j++) {
+                if(checkBoxes[j].checked == true) {
+                    currentParameters += checkBoxes[j].id + "+"
+                }
+            }
+            console.log(url)
+            url = "/show/" + myType + "/" + currentParameters.substr(0, currentParameters.length-1);
+            window.location.replace(url)
+        })
+     }
 }
-
+window.onload = showByFilters;
